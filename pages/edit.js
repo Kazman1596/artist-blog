93c56e3991b4295e0d82a3b1233ca31f1eacdbdf
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-01-21 15:55:17 -0700

    styled edit page and added image select & image preview

--- pages/edit.js
+++ pages/edit.js
@@ -2,6 +2,8 @@ import {React, useState, useRef } from 'react';
 import Menu from '../components/Menu';
 import { collection, addDoc, serverTimestamp, updateDoc, doc } from 'firebase/firestore';
 import { db, storage } from '../firebase'
+import { CameraAltOutlined } from '@mui/icons-material';
+import Image from 'next/image';
 
 // try {
 //     const docRef = addDoc(collection(db, "users"), {
@@ -16,6 +18,20 @@ import { db, storage } from '../firebase'
 
 export default function Edit() {
     const [loading, setLoading] = useState(false)
+    const [selectedFile, setSelectedFile] = useState(null)
+    
+    function addImageToPost(e) {
+        const reader = new FileReader()
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result)
+        }
+    }
+    
+    const filePickerRef = useRef(null)
 
     async function uploadPost() {
 
@@ -33,32 +49,41 @@ export default function Edit() {
     const descriptionRef = useRef(null)
 
     return (
-        <div className='bg-black'>
+        <div className='bg-black w-screen h-screen'>
             <Menu />
-            <div className=''>
-                <form>
-                    <input 
-                        type='text' 
-                        name="title" 
-                        id="title" 
-                        placeholder="Title"
-                        ref={titleRef}
-                    />
-                    <input
-                        type='text' 
-                        name='description' 
-                        id='description' 
-                        placeholder="Write something here..." 
-                        ref={descriptionRef}
-                    />
-                    <button
-                        disabled = {loading}
-                        className='text-white' 
-                        onClick={uploadPost}>
-                            Upload
-                    </button>
-                </form>
-
+            <div className='flex justify-center'>
+                {selectedFile ? (
+                    <Image onClick={() => setSelectedFile(null)} src={selectedFile} width={300} height={300} alt='selected_image' />
+                ) : (<CameraAltOutlined onClick={() => filePickerRef.current.click()} className='text-5xl m-2 p-1 text-white cursor-pointer border border-black hover:border-white hover:text-sky-500 duration-200' />)}
+                <input type='file' hidden ref={filePickerRef} onChange={addImageToPost} />
+            </div>
+            <div className='flex justify-center m-5'>
+                <input 
+                    className='w-[400px]'
+                    type='text' 
+                    name="title" 
+                    id="title" 
+                    placeholder="Title"
+                    ref={titleRef}
+                />
+            </div>
+            <div className='flex justify-center m-5'>
+                <input
+                    className='w-[500px]'
+                    type='text' 
+                    name='description' 
+                    id='description' 
+                    placeholder="Write something here..." 
+                    ref={descriptionRef}
+                />
+            </div>
+            <div className='flex justify-center m-5'>
+                <button
+                    disabled = {loading}
+                    className='text-white cursor-pointer border p-1 border-black hover:border-white hover:text-sky-500 duration-200' 
+                    onClick={uploadPost}>
+                        Upload
+                </button>
             </div>
         </div>
     );
